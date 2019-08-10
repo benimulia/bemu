0280c9b3cb7ef9b361a48d4270ea11d2db4b42c4
didimus candra <didimus.candra@si.ukdw.ac.id>
2019-08-11 00:24:32 +0800

    ssdff

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,11 +1,7 @@
 <nav class="site-navigation d-flex justify-content-end align-items-center">
     <ul class="d-flex flex-column flex-lg-row justify-content-lg-end align-content-center">
         <li class="{{Request::is('/')?'current-menu-item':''}}"><a href="/">Home</a></li>
-<<<<<<< HEAD
         <li class="{{Request::is('about')?'current-menu-item':''}}"><a href="/aboutus">About us</a></li>
-=======
-        <li class="{{Request::is('about')?'current-menu-item':''}}"><a href="/about">About us</a></li>
->>>>>>> 347b7a24fff05996f83026e3dc1dc7522ef76163
         <li class="{{Request::is('news')?'current-menu-item':''}}"><a href="/news">News</a></li>
         <li class="{{Request::is('organisasi')?'current-menu-item':''}}"><a href="/organisasi">Organization</a></li>
         <li class="{{Request::is('downloadcenter')?'current-menu-item':''}}"><a href="/downloadcenter">Download Center</a></li>
--- resources/views/user/organisasi.blade.php
+++ resources/views/user/organisasi.blade.php
@@ -1,20 +1,7 @@
 @extends('layouts.layout')
 
 @section('nav')
-<<<<<<< HEAD
-<nav class="site-navigation d-flex justify-content-end align-items-center">
-    <ul class="d-flex flex-column flex-lg-row justify-content-lg-end align-content-center">
-        <li><a href="/">Home</a></li>
-        <li><a href="/aboutus">About us</a></li>
-        <li><a href="/news">News</a></li>
-        <li class="current-menu-item"><a href="/organisasi">Organization</a></li>
-        <li><a href="/downloadcenter">Download Center</a></li>
-        <li><a href="contact.html">Contact</a></li>
-    </ul>
-</nav><!-- .site-navigation -->
-=======
 @include('layouts.navbar')<!-- .site-navigation -->
->>>>>>> 347b7a24fff05996f83026e3dc1dc7522ef76163
 @endsection
 
 @section('bodyclass', 'single-page news-page')
